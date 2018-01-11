3bd8e9dae27dd3890b3fbc0a01e93a79b34cdf33
Cwander83 <chriswandermail@gmail.com>
2018-01-11 17:57:48 -0500

    1-11-18 (got movie working a little)

--- liri.js
+++ liri.js
@@ -6,23 +6,19 @@ const request = require("request");
 const fs = require("fs");
 
 var userInput = process.argv[2];
-var songInput = process.argv[3];
+var Input = process.argv[3];
 
 fs.readFile("random.txt", "utf8", function (error, data) {
     if (error) {
-        return console.log("readdfile: " +error);
-    }
-    else if (userInput === "my-tweets"){
+        return console.log("readfile: " + error);
+    } else if (userInput === "my-tweets") {
         console.log(Tweets());
-    }
-    else if (userInput === "spotify-this-song"){
+    } else if (userInput === "spotify-this-song") {
         console.log(spotifySearch());
+    } else if (userInput === "movie-this") {
+        console.log(movieSearch());
     }
-
-    
 });
-
-
 
 // functions ------------------------------------
 
@@ -38,9 +34,10 @@ function Tweets() {
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
             for (let i = 0; i < 20; i++) {
-
-                console.log("#" + i + ": "+ tweets[i].created_at);
+                console.log("");
+                console.log("#" + i + ": " + tweets[i].created_at);
                 console.log(tweets[i].text);
+                console.log("");
                 console.log("=======================");
                 // console.log(materials.map(material => material.length));
             }
@@ -52,16 +49,40 @@ function Tweets() {
 // function for spotify, to search for songs,
 function spotifySearch() {
     const spotify = new Spotify(keys.spotify);
-    
+
     spotify.search({
         type: 'track',
-        query: songInput
+        query: Input,
+        limit: 5
     }, function (err, data) {
-        let input = data.tracks.items[0];
+        let song = data.tracks.items[0];
         if (err) {
             return console.log('Error occurred: ' + err);
-        }
-        console.log("function spotify: " + input.name);
+        };
+        console.log(data.tracks.href);
+        console.log("function spotify: " + song.name);
+        console.log("function spotify: " + song.album.name);
+        console.log("function spotify: " + song.href);
     });
+
 }
 //function for the movie search----------
+function movieSearch() {
+    request("http://www.omdbapi.com/?t=" + Input + "&y=&plot=short&apikey=83c5fdcd", function (error, response, body) {
+
+        movie = JSON.parse(body);
+        if (!error) {
+            console.log("Movie Name: " + movie.Title);
+            console.log("Release Year: " + movie.Year);
+            console.log("Actors: " + movie.Actors);
+            console.log("Language: " + movie.Language);
+            console.log("Country where the movie was produced: " + movie.Country);
+            console.log("imdbRating: " + movie.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + movie.Ratings[1].Value);
+            console.log("Plot: " + movie.Plot);
+        }
+        console.log(object);
+
+    });
+
+}
