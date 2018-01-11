46afee9f8a60813639ed75604b07af7c30a108ec
Cwander83 <chriswandermail@gmail.com>
2018-01-11 15:33:44 -0500

    1-11-18 (got spotify to work a little)

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log('keys page is connected');
+//console.log('keys page is connected');
 
 exports.twitter = {
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
--- liri.js
+++ liri.js
@@ -6,31 +6,40 @@ const request = require("request");
 const fs = require("fs");
 
 var userInput = process.argv[2];
-var songInput = "";
-
+var songInput = process.argv[3];
 
 fs.readFile("random.txt", "utf8", function (error, data) {
     if (error) {
-        return console.log(error);
+        return console.log("readdfile: " +error);
+    }
+    else if (userInput === "my-tweets"){
+        console.log(Tweets());
+    }
+    else if (userInput === "spotify-this-song"){
+        console.log(spotifySearch());
     }
 
-    console.log(Tweets());
+    
 });
+
+
+
 // functions ------------------------------------
+
 //function for twitter to find the last 20 tweets
 //from the dummy account i created
 function Tweets() {
     const client = new Twitter(keys.twitter);
     var params = {
         screen_name: 'chriswandermail',
-        count: 20
+        count: 21
     };
 
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
-            for (var i = 0; i <= 20; i++) {
+            for (let i = 0; i < 20; i++) {
 
-                console.log("#" + i + ": ");
+                console.log("#" + i + ": "+ tweets[i].created_at);
                 console.log(tweets[i].text);
                 console.log("=======================");
                 // console.log(materials.map(material => material.length));
@@ -40,16 +49,19 @@ function Tweets() {
         }
     });
 }
-
+// function for spotify, to search for songs,
 function spotifySearch() {
     const spotify = new Spotify(keys.spotify);
+    
     spotify.search({
         type: 'track',
-        query: 'All the Small Things'
+        query: songInput
     }, function (err, data) {
+        let input = data.tracks.items[0];
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        console.log("function spotify: " + data);
+        console.log("function spotify: " + input.name);
     });
 }
+//function for the movie search----------
